eb4f0ffb33d809e96e6ea15a8f8824ae3b3cae12
조하운 <79410456+henrynoowah@users.noreply.github.com>
2022-05-07 14:23:53 +0900

    [UPDATE] modal reducer

--- src/store/actions/modals.ts
+++ src/store/actions/modals.ts
@@ -2,4 +2,7 @@ import { ActionType, createAction } from 'typesafe-actions'
 import { ModalContents } from '../../models/modals'
 
 export const setModalConfirm = createAction('SET_MODAL_CONFIRM')<ModalContents | null>()
+
+export const showModal = createAction('SHOW_MODAL')<boolean>()
+
 export type ModalActionType = ActionType<typeof setModalConfirm>
